80f88138b7651666006df6076d163d985db9bd4e
zmhsena <2482811513@qq.com>
2024-11-22 12:04:11 +0800

    实现了僵尸波次的自由增删和进度设置，在GridScript中添加ReleaseGrid方法以在植物被吃后地块可重新种植

--- Lecture_DEMO/Assets/Scripts/Class3/Plant/Plant.cs
+++ Lecture_DEMO/Assets/Scripts/Class3/Plant/Plant.cs
@@ -5,19 +5,19 @@ using UnityEngine.UI;
 
 public abstract class Plant : MonoBehaviour
 {
-    protected  string plantName; // 植物名称
+    protected string plantName; // 植物名称
     protected int health; // 生命值
     protected int attackDamage; // 攻击伤害
     protected float attackCooldown; // 攻击冷却时间
     protected float attackRange; // 攻击范围
-    public bool isAlive=true ; // 植物是否存活
+    public bool isAlive = true; // 植物是否存活
 
     private float attackTimer; // 攻击冷却计时器
-    
+
 
     protected virtual void Start()
     {
-        SetAnimator("Animation/Plants/"+plantName+"/"+plantName);
+        SetAnimator("Animation/Plants/" + plantName + "/" + plantName);
     }
 
     public void TakeDamage(int damage)
@@ -31,19 +31,18 @@ public abstract class Plant : MonoBehaviour
             }
         }
     }
-    
+
     private void SetAnimator(string animatorPath)
     {
         RuntimeAnimatorController runTimeAnimator = Resources.Load<RuntimeAnimatorController>(animatorPath);
 
         Animator animator = gameObject.GetComponent<Animator>();
-        
-        if (animator == null )
-            animator=gameObject.AddComponent<Animator>();
 
-        if (runTimeAnimator != null) 
+        if (animator == null)
+            animator = gameObject.AddComponent<Animator>();
+
+        if (runTimeAnimator != null)
             animator.runtimeAnimatorController = runTimeAnimator;
-        
     }
 
 
@@ -52,13 +51,15 @@ public abstract class Plant : MonoBehaviour
         isAlive = false;
 
         Animator animator = gameObject.GetComponent<Animator>();
-        if (animator == null )
-            animator=gameObject.AddComponent<Animator>();
+        if (animator == null)
+            animator = gameObject.AddComponent<Animator>();
         animator.runtimeAnimatorController = null;
 
         Image image = gameObject.GetComponent<Image>();
-        image.color=new Color(255, 255, 255, 0f);
-        
+        image.color = new Color(255, 255, 255, 0f);
+        GridScript grid = gameObject.GetComponent<GridScript>();
+        if (grid)
+            grid.ReleaseGrid();
         Destroy(this);
     }
 
@@ -76,13 +77,12 @@ public abstract class Plant : MonoBehaviour
         {
             // 攻击冷却结束，可以执行攻击
             Attack();
-            attackTimer = 0f; 
+            attackTimer = 0f;
         }
     }
 
     // 植物攻击行为（可以根据需要重写）
     protected virtual void Attack()
     {
-
     }
 }
